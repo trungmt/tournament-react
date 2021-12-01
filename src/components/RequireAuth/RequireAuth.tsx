d942fbb4caef95ee9f87a7d8bb30f90d176b516b
trungmt <trungtran.cs@gmail.com>
2021-12-01 09:59:14 +0700

    display spinner for RequireAuth component in case isLogout = false to wait for auth context to refresh token

--- src/components/RequireAuth/RequireAuth.tsx
+++ src/components/RequireAuth/RequireAuth.tsx
@@ -1,3 +1,4 @@
+import { CircularProgress, LinearProgress } from '@mui/material';
 import { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
@@ -7,10 +8,14 @@ interface RequireAuthProps {
 }
 
 export default function RequireAuth({ children }: RequireAuthProps) {
-  const { accessToken } = useContext(AuthContext);
+  const { accessToken, isLogout } = useContext(AuthContext);
   let location = useLocation();
-  if (!accessToken) {
+  if (!accessToken && isLogout === true) {
     return <Navigate to="/admin/login" state={{ from: location }} />;
+  }
+
+  if (!accessToken && isLogout === false) {
+    return <LinearProgress sx={{ margin: '0 auto' }} />;
   }
 
   return children;

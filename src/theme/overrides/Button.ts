23d086df17e7e1bf8660e2561e26029e2367b902
trungmt <trungtran.cs@gmail.com>
2021-12-03 21:51:05 +0700

    add theme config, override components

---
+++ src/theme/overrides/Button.ts
@@ -0,0 +1,44 @@
+// ----------------------------------------------------------------------
+
+import { Theme } from '@mui/material/styles';
+
+export default function Button(theme: Theme) {
+  return {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          '&:hover': {
+            boxShadow: 'none',
+          },
+        },
+        sizeLarge: {
+          height: 48,
+        },
+        containedInherit: {
+          color: theme.palette.grey[800],
+          boxShadow: theme.customShadows.z8,
+          '&:hover': {
+            backgroundColor: theme.palette.grey[400],
+          },
+        },
+        containedPrimary: {
+          boxShadow: theme.customShadows.z1,
+        },
+        containedSecondary: {
+          boxShadow: theme.customShadows.z1,
+        },
+        outlinedInherit: {
+          border: `1px solid ${theme.palette.grey[400]}`,
+          '&:hover': {
+            backgroundColor: theme.palette.action.hover,
+          },
+        },
+        textInherit: {
+          '&:hover': {
+            backgroundColor: theme.palette.action.hover,
+          },
+        },
+      },
+    },
+  };
+}

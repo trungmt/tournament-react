e9f4852df0f2a12a58cb8fec4d4a736ca97f46ba
trungmt <trungtran.cs@gmail.com>
2021-12-04 21:51:09 +0700

    separate team table header

---
+++ src/components/table/EnhancedTableHead.tsx
@@ -0,0 +1,47 @@
+import { ChangeEvent } from 'react';
+import { TableHead, TableRow, TableCell, Checkbox } from '@mui/material';
+
+export interface HeadCell {
+  disablePadding: boolean;
+  id: string;
+  label: string;
+  numeric: boolean;
+}
+
+interface EnhancedTableProps {
+  headCells: readonly HeadCell[];
+  numSelected: number;
+  onSelectAllClick: (event: ChangeEvent<HTMLInputElement>) => void;
+  rowCount: number;
+}
+
+export default function EnhancedTableHead(props: EnhancedTableProps) {
+  const { onSelectAllClick, headCells, numSelected, rowCount } = props;
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              'aria-label': 'select all desserts',
+            }}
+          />
+        </TableCell>
+        {headCells.map(headCell => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+          >
+            {headCell.label}
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}

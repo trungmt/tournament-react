b80e93a35f99c098506b26d40d75c0627dc097e4
trungmt <trungtran.cs@gmail.com>
2021-12-16 16:16:47 +0700

    feat<axios>: add interceptors to admin client

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import constants from './constants';
 
-export const axiosClient = (accessToken: string) =>
-  axios.create({
+export const axiosClient = (accessToken: string) => {
+  const axiosAdminClient = axios.create({
     baseURL: constants.ADMIN_API_URL,
     headers: {
       'Content-Type': 'application/json',
@@ -10,6 +10,18 @@ export const axiosClient = (accessToken: string) =>
     },
     withCredentials: true,
   });
+
+  axiosAdminClient.interceptors.response.use(
+    res => {
+      return res;
+    },
+    error => {
+      // TODO: handle error connect https://axios-http.com/docs/handling_errors
+      throw error;
+    }
+  );
+  return axiosAdminClient;
+};
 
 export const axiosAuthClient = axios.create({
   baseURL: constants.AUTH_API_URL,
